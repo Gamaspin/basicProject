937da191a4d5b0020c00ebf81d757ba73bd4eab9
Gamaspin <rhrn911@gmail.com>
2022-01-08 23:15:06 +0900

    #1) 기본 포맷

---
+++ src/main/java/co/kr/controller/MainController.java
@@ -0,0 +1,12 @@
+package co.kr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String mainController(){
+        return "index";
+    }
+}
